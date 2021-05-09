fc8cf2948068b5b9a508cb23dd6ae1177f38c877
sandwitch1234 <zaq2666@gmail.com>
2021-05-09 19:21:04 +0800

    修正

--- parallax-scroll-image-full-web/README.markdown
+++
@@ -1,5 +0,0 @@
-# parallax scroll image full web
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/nguyenvan/pen/ZwvbEa](https://codepen.io/nguyenvan/pen/ZwvbEa).
-
-
--- parallax-scroll-image-full-web/src/script.js
+++
@@ -1,7 +0,0 @@
-// $('.p-content01').css({'background-size':'cover'});
-// $(window).scroll(function(){
-// 	var scrollPos = $(this).scrollTop();
-// 	$(".p-content01").css({
-// 		'background-size': 100 + scrollPos + '%' + '' + 10 + scrollPos + '%' 
-// 	});
-// });
