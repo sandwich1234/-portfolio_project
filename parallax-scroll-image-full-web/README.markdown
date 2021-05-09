be2fea9de306db2f8e82b08163bfb0005b2431d4
sandwitch1234 <zaq2666@gmail.com>
2021-05-09 19:22:53 +0800

    最終版1

---
+++ parallax-scroll-image-full-web/README.markdown
@@ -0,0 +1,5 @@
+# parallax scroll image full web
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nguyenvan/pen/ZwvbEa](https://codepen.io/nguyenvan/pen/ZwvbEa).
+
+
---
+++ parallax-scroll-image-full-web/src/script.js
@@ -0,0 +1,7 @@
+// $('.p-content01').css({'background-size':'cover'});
+// $(window).scroll(function(){
+// 	var scrollPos = $(this).scrollTop();
+// 	$(".p-content01").css({
+// 		'background-size': 100 + scrollPos + '%' + '' + 10 + scrollPos + '%' 
+// 	});
+// });
